521f61db1ef262466583e401b65e69b7a512a02d
Nandun Dasanayake <nandun@onecodelabs.com>
2026-03-31 16:39:15 +0530

    Fix domain connecting issue

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
             }
           />
 
-          {/* Admin-only routes */}
+          {/* Admin-only routes*/}
           <Route
             element={
               <ProtectedRoute allowedRoles={['admin']}>
